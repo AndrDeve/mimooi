22320fa2cf24089e072138ae0b66e56262a7531c
Andrés <marlon_rivera95@hotmail.com>
2023-02-28 12:05:13 -0500

    jeje1

---
+++ js/comprar-plata-925.js
@@ -0,0 +1,200 @@
+// PLATA  925
+document.querySelector("#submitsp5").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let refsp5 = document.querySelector("#refsp5").textContent;
+let sp5 = document.querySelector("#sp5").textContent;
+let preciosp5 = document.querySelector("#preciosp5").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a Mimooi_*%0A%0A
+*Sección Plata 925*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${refsp5}%0A%0A
+*Nombre del producto:*%0D
+${sp5}%0A%0A
+*Precio:*%0D
+${preciosp5}%0A%0A`;
+
+window.open(url);    
+});
+
+
+document.querySelector("#submitsp6").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let refsp6 = document.querySelector("#refsp6").textContent;
+let sp6 = document.querySelector("#sp6").textContent;
+let preciosp6 = document.querySelector("#preciosp6").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a Mimooi_*%0A%0A
+*Sección Plata 925*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${refsp6}%0A%0A
+*Nombre del producto:*%0D
+${sp6}%0A%0A
+*Precio:*%0D
+${preciosp6}%0A%0A`;
+
+window.open(url);    
+});
+
+
+document.querySelector("#submitsp2").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let refsp2 = document.querySelector("#refsp2").textContent;
+let sp2 = document.querySelector("#sp2").textContent;
+let preciosp2 = document.querySelector("#preciosp2").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a Mimooi_*%0A%0A
+*Sección Plata 925*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${refsp2}%0A%0A
+*Nombre del producto:*%0D
+${sp2}%0A%0A
+*Precio:*%0D
+${preciosp2}%0A%0A`;
+
+window.open(url);    
+});
+
+
+document.querySelector("#submitsp3").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let refsp3 = document.querySelector("#refsp3").textContent;
+let sp3 = document.querySelector("#sp3").textContent;
+let preciosp3 = document.querySelector("#preciosp3").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a Mimooi_*%0A%0A
+*Sección Plata 925*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${refsp3}%0A%0A
+*Nombre del producto:*%0D
+${sp3}%0A%0A
+*Precio:*%0D
+${preciosp3}%0A%0A`;
+
+window.open(url);    
+});
+
+
+document.querySelector("#submitap4").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let refap4 = document.querySelector("#refap4").textContent;
+let ap4 = document.querySelector("#ap4").textContent;
+let precioap4 = document.querySelector("#precioap4").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a Mimooi_*%0A%0A
+*Sección Plata 925*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${refap4}%0A%0A
+*Nombre del producto:*%0D
+${ap4}%0A%0A
+*Precio:*%0D
+${precioap4}%0A%0A`;
+
+window.open(url);    
+});
+
+
+document.querySelector("#submit6").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let ref6 = document.querySelector("#ref6").textContent;
+let n6 = document.querySelector("#n6").textContent;
+let precio6 = document.querySelector("#precio6").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a KAOG Accesorios_*%0A%0A
+*Sección aretes*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${ref6}%0A%0A
+*Nombre del producto:*%0D
+${n6}%0A%0A
+*Precio:*%0D
+${precio6}%0A%0A`;
+
+window.open(url);    
+});
+
+
+document.querySelector("#submit7").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let ref7 = document.querySelector("#ref7").textContent;
+let n7 = document.querySelector("#n7").textContent;
+let precio7 = document.querySelector("#precio7").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a KAOG Accesorios_*%0A%0A
+*Sección aretes*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${ref7}%0A%0A
+*Nombre del producto:*%0D
+${n7}%0A%0A
+*Precio:*%0D
+${precio7}%0A%0A`;
+
+window.open(url);    
+});
+
+
+document.querySelector("#submit8").addEventListener("click", e => {
+e.preventDefault();
+
+//INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+let telefono = "573172171691";
+
+let ref8 = document.querySelector("#ref8").textContent;
+let n8 = document.querySelector("#n8").textContent;
+let precio8 = document.querySelector("#precio8").textContent;
+
+let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+*_Bienvenidos a KAOG Accesorios_*%0A%0A
+*Sección aretes*%0A%0A
+*Tu compra:*%0A%0A
+*Ref del producto:*%0D
+${ref8}%0A%0A
+*Nombre del producto:*%0D
+${n8}%0A%0A
+*Precio:*%0D
+${precio8}%0A%0A`;
+
+window.open(url);    
+});
+    
